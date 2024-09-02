b1cd1e38a8c0c369d95339dc5d7f65210fff705a
Wesley-waka <wesleywaka77@gmail.com>
2024-09-03 00:16:21 +0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,46 @@
+import Aura from '@primevue/themes/aura';
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  css: [
+    '@/assets/css/tailwind.css',
+    'primeicons/primeicons.css'
+  ],
+  modules: [
+    '@primevue/nuxt-module',
+    '@nuxtjs/tailwindcss'
+  ],
+  plugins: ["~/plugins/preline.client.ts"],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: '.p-dark',
+          cssLayer: false
+        }
+      }
+    },
+    components: {
+      prefix: '',
+      exclude: []
+    }
+  },
+  tailwindcss: {
+    cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }],
+    configPath: 'tailwind.config',
+    exposeConfig: {
+      level: 2
+    },
+    config: {},
+    viewer: true,
+  },
+  app: {
+    head: {
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Pacifico&display=swap' }
+      ]
+    }
+  }
+})
